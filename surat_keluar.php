e9340f3be9d253ab9a0abf485aa2a6a2a54afa03
krisnaaditya178 <krisnaadityaa178@gmail.com>
2025-03-04 16:02:57 +0700

    updated project files

--- surat_keluar.php
+++ surat_keluar.php
@@ -340,6 +340,15 @@ $surat_masuk_data = $stmt->fetchAll();
             <p>Tunggu Sebentar....</p>
         </div>
 
+                    <!-- Modal untuk menampilkan file -->
+                    <div id="fileModal" class="modal-file">
+        <div class="modal-content-file">
+            <span class="close-btn-file" onclick="closeModal()">&times;</span>
+            <h3>Pratinjau File</h3>
+            <div id="filePreview"></div>    
+        </div>
+    </div>
+
     <!-- Footer -->
     <footer>
         <p>&copy; 2025 Arsip Surat - Nadia Aprilia Siregar</p>
@@ -347,30 +356,25 @@ $surat_masuk_data = $stmt->fetchAll();
 
     <script>
         // Get the modal
-        var modal = document.getElementById("addModal");
-
-        // Get the button that opens the modal
+        document.addEventListener('DOMContentLoaded', function() {
         var btn = document.getElementById("openModalBtn");
-
-        // Get the <span> element that closes the modal
+        var modal = document.getElementById("addModal");
         var span = document.getElementsByClassName("close-btn-out")[0];
 
-        // When the user clicks on the button, open the modal
         btn.onclick = function() {
             modal.style.display = "block";
         }
 
-        // When the user clicks on <span> (x), close the modal
         span.onclick = function() {
             modal.style.display = "none";
         }
 
-        // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
             }
         }
+    });
 
                 // Fungsi untuk menampilkan loading spinner
                 function showLoading() {
@@ -421,7 +425,7 @@ $surat_masuk_data = $stmt->fetchAll();
             var fileExtension = filePath.split('.').pop().toLowerCase();
 
             if (['jpg', 'jpeg', 'png'].includes(fileExtension)) {
-                filePreview.innerHTML = '<img src="${filePath}" style="max-width: 100%; height: auto;" alt="File Preview">';
+                filePreview.innerHTML = `<img src="${filePath}" style="max-width: 100%; height: auto;" alt="File Preview">`;
             } else if (fileExtension === 'pdf') {
                 filePreview.innerHTML = '<iframe src="${filePath}" width="100%" height="500px" style="border: none;"></iframe>';
             } else {
